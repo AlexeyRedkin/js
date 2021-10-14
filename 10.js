19628b28887365cabee1a05779b1c3b281440063
Redkin Alexey <C:\Users\redki\AppData\Roaming\The Bat!>
2021-10-14 17:00:55 +0600

    js

--- 10.js
+++ 10.js
@@ -1,5 +1,5 @@
 function isPrime(n) {
-            if (!Number.isInteger(n) || n < 2 || n > 1000) return "данные неверны";
+            if (!Number.isInteger(n) || n < 0 || n > 1000) return "данные неверны";
  
             let k = Math.sqrt(n);
             for (let i = 2; i <= k; i++)
@@ -8,5 +8,6 @@ function isPrime(n) {
             return "простое число";
         }
  
+        console.log(isPrime(999));
+        console.log(isPrime(0));
         console.log(isPrime(1555));
-        console.log(isPrime(7));
